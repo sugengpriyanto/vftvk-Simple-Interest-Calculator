df01fad3b591126ff0c2d43b19662023f97ccd56
Sugeng Priyanto <54300975+sugengpriyanto@users.noreply.github.com>
2021-12-20 08:54:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    // get form value
+    amount = document.getElementById("principal").value;
+    rate = parseFloat(document.getElementById("rate").value).toFixed(1);
+    years = parseInt(document.getElementById('years').value);
+
+    // compute result
+    result = amount * (rate / 100) * years
+
+    // get result date
+    now = new Date().getFullYear()
+    year = now + years
+
+    // debug
+    console.log(rate / 100)
+
+    // validate
     
+    if (amount <= 0) {
+        // show validation notice
+        document.getElementById('amountVal').classList.remove('hidden')
+    } else {
+        document.getElementById('amountVal').classList.add('hidden')
+        document.getElementById('amount').innerText = amount
+        document.getElementById('rateValue').innerText = rate
+        document.getElementById('result').innerText = result
+        document.getElementById('year').innerText = year
+        document.getElementById('results').classList.remove('hidden')
+    }
 }
-        
+
+function updateTextInput(val) {
+    document.getElementById('range').innerText = val
+}
